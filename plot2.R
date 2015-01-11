798ade5c6491db440b63560cf911dfa0738dd227
dkshstn1 <dkshstn1@users.noreply.github.com>
2015-01-11 03:18:46 -0600

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,19 @@
+#READ DATA
+data2007 <- subset(data, data$Date=="1/2/2007" | data$Date=="2/2/2007")
+
+#SUBSET DATA FOR 2007 FEB 1 AND 2
+data2007$Time <- paste(data2007$Date, data2007$Time)
+
+#CONVERT TIME TO POSIX
+data2007$Time <- strptime(data2007$Time, format="%d/%m/%Y %H:%M:%S")
+
+#SET GLOBAL ACTIVE POWER AS NUMERIC
+data2007$Global_active_power <- as.numeric(as.character(data2007$Global_active_power))
+
+#CREATE PNG FILE
+png(file="plot2.png", 480, 480)
+
+#PLOT LINE GRAPH
+plot(data2007$Time, data2007$Global_active_power, xlab="", ylab = "Global Active Power (kilowatts)", type="l")
+
+dev.off()
